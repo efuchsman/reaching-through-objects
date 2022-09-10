f75f134aaf650815e9d275920cdfe3cb4b712e2d
Eli Fuchsman <efuchsman@gmail.com>
2022-09-10 17:47:59 -0600

    Create attributes test

--- exercise_1/spec/building_spec.rb
+++ exercise_1/spec/building_spec.rb
@@ -9,7 +9,12 @@ RSpec.describe Building do
     expect(bldg).to be_an_instance_of Building
   end
 
+  it 'has readable attributes is_rented by default' do
+    bldg = Building.new("623", "Savills Apartment Building")
 
+    expect(bldg.building_number).to eq("623")
+    expect(bldg.building_name).to eq("Savills Apartment Building")
+  end
 
 
 
