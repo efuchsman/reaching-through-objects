21e1e1fcc012c91317d073d2f05e47a2f6bd9b2e
Eli Fuchsman <efuchsman@gmail.com>
2022-09-11 14:48:18 -0600

    Initialize street class

---
+++ exercise_1/lib/street.rb
@@ -0,0 +1,9 @@
+class Street
+attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+end
---
+++ exercise_1/spec/street_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/room.rb'
+require './lib/apartment.rb'
+require './lib/building.rb'
+require './lib/street.rb'
+
+RSpec.describe do Street
+  it 'exists' do
+    adlington = Street.new("Adlington Road")
+
+    expect(adlington).to be_an_instance_of(Street)
+  end
+
+
+end
