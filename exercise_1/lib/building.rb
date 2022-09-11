8b78f57d3d2026a00cf5fbaeabbe4cdd0a8dc4c5
Eli Fuchsman <efuchsman@gmail.com>
2022-09-11 14:37:48 -0600

    Create method and tests for adding apartments

--- exercise_1/lib/building.rb
+++ exercise_1/lib/building.rb
@@ -1,8 +1,18 @@
 class Building
-attr_reader :building_name, :building_number
+attr_reader :building_name, :building_number, :apartments
   def initialize(building_number, building_name)
     @building_number = building_number
     @building_name =building_name
+    @apartments = []
+  end
+
+  def add_apartment(apt)
+    @apartment = apt
+    @apartments << apt
+  end
+
+  def list_apartments
+    @apartments
   end
 
 end
--- exercise_1/spec/building_spec.rb
+++ exercise_1/spec/building_spec.rb
@@ -16,7 +16,18 @@ RSpec.describe Building do
     expect(bldg.building_name).to eq("Savills Apartment Building")
   end
 
+  it 'can have up to 4 apartments' do
+    bldg = Building.new("623", "Savills Apartment Building")
+    apt_1 = Apartment.new
+    apt_2 = Apartment.new
+    apt_5 = Apartment.new
 
+    bldg.add_apartment(apt_1)
+    bldg.add_apartment(apt_2)
+    bldg.add_apartment(apt_5)
+
+      expect(bldg.list_apartments).to eq ([apt_1, apt_2, apt_5])
+  end
 
 
 end
