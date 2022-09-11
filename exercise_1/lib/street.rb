41b7c9b5dd825db7a4606771df7368e798bb0ea8
Eli Fuchsman <efuchsman@gmail.com>
2022-09-11 15:19:10 -0600

    Create method for displaying building names alphabetically

--- exercise_1/lib/street.rb
+++ exercise_1/lib/street.rb
@@ -1,9 +1,27 @@
 class Street
-attr_reader :name
+attr_reader :name, :buildings_array
 
   def initialize(name)
     @name = name
+    @buildings_array = []
   end
 
+  def add_building(bldg)
+    @building = bldg
+    @buildings_array << bldg
+  end
+
+  def buildings
+    buildings_alphabetically = []
+
+    @buildings_array.each do |bldg|
+      bldg_name = bldg.building_name
+      buildings_alphabetically << bldg_name
+    end
+      sorted =buildings_alphabetically.sort_by do |bldg_name|
+        bldg_name.chars
+      end
+      sorted
+  end
 
 end
--- exercise_1/spec/street_spec.rb
+++ exercise_1/spec/street_spec.rb
@@ -17,6 +17,16 @@ RSpec.describe do Street
     expect(adlington.name).to eq("Adlington Road")
   end
 
+    it "has buildings" do
+      adlington = Street.new("Adlington Road")
+      zebra = Building.new("123", "Zebra Apartments")
+      savills = Building.new("623", "Savills Apartment Building")
 
+      adlington.add_building(zebra)
+      adlington.add_building(savills)
+
+      expect(adlington.buildings).to eq(["Savills Apartment Building", "Zebra Apartments"])
+
+    end
 
 end
